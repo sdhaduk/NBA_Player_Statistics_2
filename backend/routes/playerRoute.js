24a07404b5f91511c39c5ef1dd64c1bb8803fa5c
sdhaduk <sagardhaduk1@gmail.com>
2023-10-23 20:46:30 -0500

    Added route that returns top x amount of players for a specified statistic

--- backend/routes/playerRoute.js
+++ backend/routes/playerRoute.js
@@ -3,13 +3,13 @@ import {Player} from "../models/playerModel.js"
 
 const router = express.Router();
 
-
 // Get a specific player
-router.get('/:name', async (req, res) => {
+router.get('/single/:name', async (req, res) => {
     try {
         const {name} = req.params;
         const player = await Player.findOne({player: name})
         return res.status(200).json(player);
+
     } catch(err) {
         console.log(err.message)
         return res.status(500).send({message: err.message})
@@ -17,13 +17,29 @@ router.get('/:name', async (req, res) => {
 });
 
 // Get all players
-router.get('/', async(req, res) => {
+router.get('/all', async(req, res) => {
     try {
         const players = await Player.find({})
         return res.status(200).json(players);
+
     } catch(err) {
         console.log(err.message)
         return res.status(500).send({message: err.message})
+    }
+});
+
+//get top x amount for selected statistic
+router.get('/stat/:statistic/:number', async (req, res) => {
+    const number = req.params.number;
+    const statistic = req.params.statistic;
+
+    try {
+        const players = await Player.find().sort({[statistic]:-1}).limit(number);
+        return res.status(200).json(players);
+
+    } catch(err) {
+        console.log(err.message)
+        return res.status(500).send({message: err.message}) 
     }
 });
 
