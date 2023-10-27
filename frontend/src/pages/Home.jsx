2291f2982c6fe0097d449591ada82cbc7b432470
sdhaduk <sagardhaduk1@gmail.com>
2023-10-26 21:40:33 -0500

    Added Loading component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,21 +2,28 @@ import React, { useState, useEffect } from "react";
 import {
   Grid,
   Typography,
+  useStepContext,
 } from "@mui/material";
 import PlayerTable from "../components/PlayerTable";
+import Loading from "../components/Loading";
 import axios from 'axios';
 
 const Home = () => {
     
     const [players, setPlayers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
+      setLoading(true)
         axios
         .get("http://localhost:3000/players/all")
         .then((response) => {
             setPlayers(response.data)
+            setLoading(false)
         })
         .catch((error) => {
             console.log(error)
+            setLoading(false)
         });
     }, []);
 
@@ -25,7 +32,7 @@ const Home = () => {
       <Grid item xs={12} sx={{ mt: 2 }}>
         <Typography variant="h2">NBA Statistics 2023</Typography>
       </Grid>
-      <PlayerTable players={players} />   
+      {loading ? <Loading /> : <PlayerTable players={players} />}
     </Grid>
   );
 };
