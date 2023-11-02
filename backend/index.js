2319c8bd30f3033a98aba8f42700ed0adcc6ea16
Sagar Dhaduk <sagardhaduk1@gmail.com>
2023-11-01 21:24:00 -0500

    Created get for '/' for Render

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,10 @@ mongoose.connect(process.env.MONGODB_URI)
     console.log(err);
 });
 
+app.get('/', (req, res) => {
+    res.status(200).json({message: "hi"})
+});
+
 app.use('/players', playerRoute);
 
 
